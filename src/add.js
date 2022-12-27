774e2abdbb3220344de06bc4c87e396da7a28979
Eyouel <eeyoueltsegaye@gmail.com>
2022-12-27 13:03:06 +0300

    created the duration setter function to calculate the reading time of the blog

--- src/add.js
+++ src/add.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useState } from 'react';   
-import blogs from "./blogs"
+import blogs from "./blogs";
+
 export default function Add(){
     const [blog, setBlog] = useState({
         title:"",
         author:"admin",
         body:"",
         tags:"",
+        duration: 1,
 
     })
     function handleChange(event){
@@ -22,7 +24,22 @@ export default function Add(){
     function handleSubmit(event){
         event.preventDefault();
         console.log("form submitted");
+        durationSetter(blog.body)
     }
+    function durationSetter(bodyText){
+        let blogLength = (bodyText.length)/4.7
+        console.log("length is ", blogLength)
+        let duration = Math.ceil(blogLength/238)
+        console.log("duration: ", duration)
+        // set the duration to the blog object and update the state
+        setBlog(Prevblog=>({
+            ...Prevblog,
+            duration: duration
+        })
+        )
+        
+        
+    };
     return(
         <div>
             <form onSubmit={handleSubmit}>
